7d9c352f561fb25e4e217c3024715138c506a166
Tsukimoto <tsukitsumoto@gmail.com>
2025-03-11 13:54:02 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <llvm/Support/raw_ostream.h>
+//this is to check later at home if it works.
+//yea im doing it on phone
+int main() {
+    llvm::outs() << "Hello, Neoluma!\n";
+    return 0;
+}
