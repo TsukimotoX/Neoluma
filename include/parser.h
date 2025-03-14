22ed4531a22e343720ec827e2b2cf275f502eabb
Tsukimoto <tsukitsumoto@gmail.com>
2025-03-14 14:16:06 +0300

    parser header added

--- include/parser.h
+++ include/parser.h
@@ -4,10 +4,39 @@
 #include "lexer.h"
 
 struct ASTNode {
-    TokenType type;
-    std::string value;
-    ASTNode* left;
-    ASTNode* right;
+    virtual ~ASTNode() = default;
 };
 
+struct LiteralNode : public ASTNode {
+    int value;
+    LiteralNode(int val) : value(val) {}
+}
+
+struct VariableNode : public ASTNode {
+    std::string n; //name
+    VariableNode(const std::string& name) : n(name) {}
+}
+
+struct BinaryOperationNode : public ASTNode {
+    std::unique_ptr<ASTNode> l; // left
+    std::string o; //operation like math or logic
+    std::unique_ptr<ASTNode> r; // right
+
+    BinaryOperationNode(std::unique_ptr<ASTNode> left, const std::string& op, std::unique_ptr<ASTNode> right) : left(std::move(l)), op(o), right(std::move(r)) {}
+}
+
+// Unary operation is an operation to do with one value (-a, !a)
+struct UnaryOperationNode : public ASTNode {
+    std::string op; //operator
+    std::unique_ptr<ASTNode> oprnd; //operand
+
+    UnaryOperationNode(const std::string& operator, std::unique_ptr<ASTNode> operand) : op(operator), oprnd(std::move(operand)) {}
+}
+
+struct FunctionCallNode : public ASTNode {
+    std::string funcName;
+    std::vector<std::unique_ptr<ASTNode>> arguments;
+
+    FunctionCallNode(const std::string& name) : funcName(name) {}
+}
 #endif 
